41d1ff51db6958f506c1f36bb5be522d4dfdab2b
DawidCiechowski <dawid.ciechowski@gmail.com>
2019-04-10 16:51:16 +0100

    Added modulo operator

--- Main.py
+++ Main.py
@@ -13,24 +13,24 @@ root.minsize(500, 250)
 
 #
 first_number_label = Label(root, text="First number: ")
-first_number_label.grid(column=1, row = 1)
+first_number_label.grid(column=0, row = 0)
 
 first_number_type = IntVar()
 first_number_input = Entry(root, textvariable=first_number_type)
-first_number_input.grid(column=2, row =1)
+first_number_input.grid(column=1, row =0)
 
 second_number_label = Label(root, text="Second number: ")
-second_number_label.grid(column=1, row=2, pady=10)
+second_number_label.grid(column=0, row=1, pady=10)
 
 second_number_type = IntVar()
 second_number_input = Entry(root, textvariable=second_number_type)
-second_number_input.grid(column=2, row = 2, pady = 10)
+second_number_input.grid(column=1, row = 1, pady = 10)
 
 result_label = Label(root, text="Result: ")
-result_label.grid(column=1, row=4, pady=10, padx = 0, ipadx=0)
+result_label.grid(column=0, row=3, sticky=W)
 
 result = Label(root, text="")
-result.grid(column=2, row = 4, sticky = W)
+result.grid(column=1, row = 3, sticky = W)
 
 
 def define_action(action):
@@ -51,6 +51,8 @@ def define_action(action):
         final_result = number_1.get() / number_2.get()
     elif(action == "DIVIDE" and number_2.get() == 0):
         mbox.showinfo("ERROR!", "Cannot divide by 0!")
+    elif(action == "MODULO"):
+        final_result = number_1.get() % number_2.get()
 
     result.configure(text=final_result)
 
@@ -66,19 +68,25 @@ def multiply_listener():
 def divide_listener():
     define_action("DIVIDE")
 
+def modulo_listener():
+    define_action("MODULO")
+
     
 
 add_button = Button(root, command = add_listener, text="+")
-add_button.grid(column=1, row=3)
+add_button.grid(column=0, row=2, sticky = W)
 
 substract_button = Button(root, command = substract_listener, text="-")
-substract_button.grid(column=2, row=3, sticky=W)
+substract_button.grid(column=0, row=2)
 
 multiply_button = Button(root, command = multiply_listener, text="*")
-multiply_button.grid(column=2, row=3)
+multiply_button.grid(column=0, row=2, sticky = E)
 
 divide_button = Button(root, command = divide_listener, text="/")
-divide_button.grid(column=2, row=3, sticky = E)
+divide_button.grid(column=1, row=2, pady = 0)
+
+modulo_button = Button(root, command = modulo_listener, text="%")
+modulo_button.grid(column=1, row=2, sticky = E)
 
 
 #Run mainloop
