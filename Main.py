85ad93bacc3dea19e6fd5f940308b7352da30b2a
DawidCiechowski <dawid.ciechowski@gmail.com>
2019-04-10 01:34:27 +0100

    Updated code, added comments

--- Main.py
+++ Main.py
@@ -1,66 +1,42 @@
 from tkinter import *
 import tkinter.messagebox
 
+#Instantiate tkinter
 root = Tk()
 
-
-
+#Add top and bottom frames to the window
 top_frame = Frame(root)
 top_frame.pack()
 
 bottom_frame = Frame(root)
-bottom_frame.pack(side=BOTTOM)
+bottom_frame.pack(side = BOTTOM)
 
-l = Label(root, text="This is a piece of text", fg="black", bg="red")
+#Instantiate the label and change colour variables
+l = Label(root, text="This is a piece of text", fg="black", bg = "blue")
+change_colour = True
 
-
+#Button listener. Change the colour of label's bg, depending on the 'change_colour' value
 def buttonClick():
     global l
-    l = Label(root, text="This is a piece of text", fg="black", bg="blue")
+    global change_colour
+    if(change_colour == True):
+        l.configure(bg = "red")
+        change_colour = False
+    else:
+        l.configure(bg = "blue")
+        change_colour = True
 
-button_1 = Button(root, command = buttonClick, text="This is just gay", fg="blue")
+#Instanitate the button with listener
+button_1 = Button(root, command = buttonClick, text="Click", fg="blue")
 
-
+#Pack everything
 l.pack()
 
 
 button_1.pack(fill=X)
 
-
+#Run mainloop
 root.mainloop()
-
-"""
-    b = True
-    l = Label(root, text = "This is a piece of text", fg = "black", bg = "blue")
-
-    def buttonCLick(self):
-        global b
-        global l
-        if(b == True):
-            l = Label(root, text = "This is a piece of text", fg = "black", bg = "red")
-            b = False
-        else:
-            l = Label(root, text = "This is a piece of text", fg = "black", bg = "blue")
-            b = True
-
-    def instantiate_variables(self):
-        top_frame = Frame(root)
-        top_frame.pack()
-        bottom_frame = Frame(root)
-        bottom_frame.pack()
-        button_1 = Button(root, command = buttonClick, text="Click!", fg="blue")
-        button_1.pack()
-        l.pack()
-
-    
-    def run(self):
-        root.mainloop()
-
-c = cs()
-
-c.instantiate_variables()
-c.run()
-"""
 
 
 
